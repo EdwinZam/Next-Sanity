7878e9821409f920392e06fbd5c84b957cfc3317
Edwin <eaespinosa9312@hotmail.com>
2023-05-14 20:53:55 -0500

    Sanity-studio + Next v13

---
+++ sanity.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
+
+const config = defineConfig({
+  projectId: "lxolyaqu",
+  dataset: "production",
+  title: "My Personal Website",
+  apiVersion: "2023-05-03",
+  basePath: "/admin",
+  plugins: [deskTool()],
+  schema: { types: schemas },
+});
+
+export default config;
---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: "lxolyaqu",
+  dataset: "production",
+  apiVersion: "2023-05-03",
+};
+
+export default config;
