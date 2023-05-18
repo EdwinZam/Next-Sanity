bd083ab687ae88dd11c680848fed246dff5a83c7
Edwin <eaespinosa9312@hotmail.com>
2023-05-17 22:59:47 -0500

    Css + Contenido + UI Home

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -22,9 +22,22 @@ export default async function Project({ params }: Props) {
           title="View Project"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-gray-100 rounded-lg text-gray-700 font-semibold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-white transition-all"
+          className="bg-gray-100 rounded-lg text-gray-700 font-semibold py-3 px-4 flex items-center space-x-2 whitespace-nowrap hover:bg-pink-500 hover:text-white transition-all"
         >
-          Ir Projecto
+          <span>View Project</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 transform scale-x-[-1]"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 1a9 9 0 1 0 0 18 9 9 0 0 0 0-18zm0 16a7 7 0 1 1 0-14 7 7 0 0 1 0 14zm-.707-5.293a1 1 0 0 1 1.414-1.414l2 2a1 1 0 1 1-1.414 1.414L9 10.414V14a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1h6a1 1 0 1 1 0 2h-3.586l2.293 2.293z"
+              clipRule="evenodd"
+            />
+          </svg>
         </a>
       </header>
 
